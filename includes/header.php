ef3baed4afac4972924e482c2326cebb4f608893
timothe belcour <belcour9u@etu.univ-lorraine.fr>
2025-11-18 16:29:16 +0100

    rajout du lien contact dans le header + création du fichier contact

--- includes/header.php
+++ includes/header.php
@@ -23,6 +23,7 @@
           <li><a href="vote.php" class="main-nav__link">Vote</a></li>
           <li><a href="resultats.php" class="main-nav__link">Résultats</a></li>
           <li><a href="login.php" class="main-nav__link">Se connecter</a></li>
+          <li><a href="contact.php" class="main-nav__link">Contact</a></li>
           <li><a href="apropos.php" class="main-nav__link">À propos</a></li>
         </ul>
       </nav>
