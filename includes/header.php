b56874dc2979e65afa9623a865553082b049314d
skullthefan-byte <skullthefan@gmail.com>
2025-12-02 11:40:18 +0100

    Changement du titre de l'onglet

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
-  <title>Votendo – Plateforme de vote GOTY</title>
+  <title>Votendo</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
