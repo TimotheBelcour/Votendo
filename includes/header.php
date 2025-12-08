1f805a847df904a39bac176f25215e962c23f274
timothe belcour <belcour9u@etu.univ-lorraine.fr>
2025-12-08 08:59:31 +0100

    Suppression de db.php obsolète

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
 // Inclure le fichier de configuration de la base de données
-include __DIR__ . '/db.php';
+include __DIR__ . '/config.php';
 
 // Démarrer la session si elle n'est pas déjà active
 if (session_status() === PHP_SESSION_NONE) {
